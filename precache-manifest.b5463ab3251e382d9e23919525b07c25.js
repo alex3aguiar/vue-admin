7889bcd3d13332eb55208248b9c38794753b093c
Your username <your email>
2020-09-04 01:50:49 +0000

    gh-pages

---
+++ precache-manifest.b5463ab3251e382d9e23919525b07c25.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "651008823fc8394ed854",
+    "url": "/vue-admin/css/app.5f0f56bc.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vue-admin/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "27d62bab76055d58b02e99de0c1fed29",
+    "url": "/vue-admin/index.html"
+  },
+  {
+    "revision": "61858f9a9b236cacc848",
+    "url": "/vue-admin/js/about.f12617ad.js"
+  },
+  {
+    "revision": "651008823fc8394ed854",
+    "url": "/vue-admin/js/app.01f762ad.js"
+  },
+  {
+    "revision": "ab03d2032c7a7bfeabb9",
+    "url": "/vue-admin/js/chunk-vendors.724bd317.js"
+  },
+  {
+    "revision": "ad1c7122216ff6e919bd68f90e80d94d",
+    "url": "/vue-admin/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-admin/robots.txt"
+  }
+]);
