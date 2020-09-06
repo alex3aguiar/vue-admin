86ab056032b37639a18426e7be9da5a6c0767333
alex <alex3aguiar@gmail.com>
2020-09-06 15:32:23 -0300

    add vue bootstrap

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import Vue from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
 Vue.config.productionTip = false;
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
 new Vue({
   router,
